90519bccaa52b29909dab42b164724932bfe94a0
Sergey Klimanoff <iamjsare@gmail.com>
2022-09-01 19:50:08 +0300

    Add google fonts and change color theme

--- flutter_bloc_apps/timer_bloc/lib/app.dart
+++ flutter_bloc_apps/timer_bloc/lib/app.dart
@@ -2,18 +2,25 @@ import 'package:flutter/material.dart';
 
 import 'timer/view/timer_page.dart';
 
+import 'package:google_fonts/google_fonts.dart';
+
 class TimerApp extends StatelessWidget {
   const TimerApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
     return MaterialApp(
       title: 'Timer App',
       theme: ThemeData(
         primaryColor: const Color.fromRGBO(109, 234, 255, 1),
-        colorScheme: const ColorScheme.light(
-          secondary: Color.fromARGB(255, 113, 118, 255),
+        textTheme: GoogleFonts.manropeTextTheme(textTheme).copyWith(
+          headline2: const TextStyle(
+              color: Colors.indigo, fontWeight: FontWeight.bold),
         ),
+        colorScheme: const ColorScheme.light(
+            secondary: Colors.indigo,
+            onSecondary: Color.fromARGB(255, 255, 255, 255)),
       ),
       home: const TimerPage(),
     );
