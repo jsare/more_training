439e006229e5af4c81167147e33ab5007fa448df
Sergey Klimanoff <iamjsare@gmail.com>
2022-09-06 17:52:11 +0300

    Add splash_page file

--- flutter_bloc_apps/auth_with_bloc/lib/app.dart
+++ flutter_bloc_apps/auth_with_bloc/lib/app.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:user_repository/user_repository.dart';
 
+import 'splash/view/splash_page.dart';
+
 class App extends StatelessWidget {
   const App({
     super.key,
